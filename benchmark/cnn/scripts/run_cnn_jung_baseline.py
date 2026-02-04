4b5d8d5b832f4ae82bbe1e2268fb3da8b5ed2906
verdiant <verdiant@student.zhaw.ch>
2026-02-04 22:22:44 +0100

    Refactor string formatting and improve code consistency in Jupyter notebooks
    - Updated string quotes from single to double for consistency across load_data.ipynb and plot_spectra.ipynb.
    - Removed unnecessary newline characters in function definitions and plot titles.
    - Enhanced readability by aligning string formatting and spacing in print statements.
    - Ensured consistent use of double quotes for dictionary keys and plot labels.

--- benchmark/cnn/scripts/run_cnn_jung_baseline.py
+++ benchmark/cnn/scripts/run_cnn_jung_baseline.py
@@ -25,43 +25,43 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 
 functional_groups = {
-    'Acid anhydride': Chem.MolFromSmarts('[CX3](=[OX1])[OX2][CX3](=[OX1])'),
-    'Acyl halide': Chem.MolFromSmarts('[CX3](=[OX1])[F,Cl,Br,I]'),
-    'Alcohol': Chem.MolFromSmarts('[#6][OX2H]'),
-    'Aldehyde': Chem.MolFromSmarts('[CX3H1](=O)[#6,H]'),
-    'Alkane': Chem.MolFromSmarts('[CX4;H3,H2]'),
-    'Alkene': Chem.MolFromSmarts('[CX3]=[CX3]'),
-    'Alkyne': Chem.MolFromSmarts('[CX2]#[CX2]'),
-    'Amide': Chem.MolFromSmarts('[NX3][CX3](=[OX1])[#6]'),
-    'Amine': Chem.MolFromSmarts('[NX3;H2,H1,H0;!$(NC=O)]'),
-    'Arene': Chem.MolFromSmarts('[cX3]1[cX3][cX3][cX3][cX3][cX3]1'),
-    'Azo compound': Chem.MolFromSmarts('[#6][NX2]=[NX2][#6]'),
-    'Carbamate': Chem.MolFromSmarts('[NX3][CX3](=[OX1])[OX2H0]'),
-    'Carboxylic acid': Chem.MolFromSmarts('[CX3](=O)[OX2H]'),
-    'Enamine': Chem.MolFromSmarts('[NX3][CX3]=[CX3]'),
-    'Enol': Chem.MolFromSmarts('[OX2H][#6X3]=[#6]'),
-    'Ester': Chem.MolFromSmarts('[#6][CX3](=O)[OX2H0][#6]'),
-    'Ether': Chem.MolFromSmarts('[OD2]([#6])[#6]'),
-    'Haloalkane': Chem.MolFromSmarts('[#6][F,Cl,Br,I]'),
-    'Hydrazine': Chem.MolFromSmarts('[NX3][NX3]'),
-    'Hydrazone': Chem.MolFromSmarts('[NX3][NX2]=[#6]'),
-    'Imide': Chem.MolFromSmarts('[CX3](=[OX1])[NX3][CX3](=[OX1])'),
-    'Imine': Chem.MolFromSmarts('[$([CX3]([#6])[#6]),$([CX3H][#6])]=[$([NX2][#6]),$([NX2H])]'),
-    'Isocyanate': Chem.MolFromSmarts('[NX2]=[C]=[O]'),
-    'Isothiocyanate': Chem.MolFromSmarts('[NX2]=[C]=[S]'),
-    'Ketone': Chem.MolFromSmarts('[#6][CX3](=O)[#6]'),
-    'Nitrile': Chem.MolFromSmarts('[NX1]#[CX2]'),
-    'Phenol': Chem.MolFromSmarts('[OX2H][cX3]:[c]'),
-    'Phosphine': Chem.MolFromSmarts('[PX3]'),
-    'Sulfide': Chem.MolFromSmarts('[#16X2H0]'),
-    'Sulfonamide': Chem.MolFromSmarts('[#16X4]([NX3])(=[OX1])(=[OX1])[#6]'),
-    'Sulfonate': Chem.MolFromSmarts('[#16X4](=[OX1])(=[OX1])([#6])[OX2H0]'),
-    'Sulfone': Chem.MolFromSmarts('[#16X4](=[OX1])(=[OX1])([#6])[#6]'),
-    'Sulfonic acid': Chem.MolFromSmarts('[#16X4](=[OX1])(=[OX1])([#6])[OX2H]'),
-    'Sulfoxide': Chem.MolFromSmarts('[#16X3]=[OX1]'),
-    'Thial': Chem.MolFromSmarts('[CX3H1](=S)[#6,H]'),
-    'Thioamide': Chem.MolFromSmarts('[NX3][CX3]=[SX1]'),
-    'Thiol': Chem.MolFromSmarts('[#16X2H]')
+    "Acid anhydride": Chem.MolFromSmarts("[CX3](=[OX1])[OX2][CX3](=[OX1])"),
+    "Acyl halide": Chem.MolFromSmarts("[CX3](=[OX1])[F,Cl,Br,I]"),
+    "Alcohol": Chem.MolFromSmarts("[#6][OX2H]"),
+    "Aldehyde": Chem.MolFromSmarts("[CX3H1](=O)[#6,H]"),
+    "Alkane": Chem.MolFromSmarts("[CX4;H3,H2]"),
+    "Alkene": Chem.MolFromSmarts("[CX3]=[CX3]"),
+    "Alkyne": Chem.MolFromSmarts("[CX2]#[CX2]"),
+    "Amide": Chem.MolFromSmarts("[NX3][CX3](=[OX1])[#6]"),
+    "Amine": Chem.MolFromSmarts("[NX3;H2,H1,H0;!$(NC=O)]"),
+    "Arene": Chem.MolFromSmarts("[cX3]1[cX3][cX3][cX3][cX3][cX3]1"),
+    "Azo compound": Chem.MolFromSmarts("[#6][NX2]=[NX2][#6]"),
+    "Carbamate": Chem.MolFromSmarts("[NX3][CX3](=[OX1])[OX2H0]"),
+    "Carboxylic acid": Chem.MolFromSmarts("[CX3](=O)[OX2H]"),
+    "Enamine": Chem.MolFromSmarts("[NX3][CX3]=[CX3]"),
+    "Enol": Chem.MolFromSmarts("[OX2H][#6X3]=[#6]"),
+    "Ester": Chem.MolFromSmarts("[#6][CX3](=O)[OX2H0][#6]"),
+    "Ether": Chem.MolFromSmarts("[OD2]([#6])[#6]"),
+    "Haloalkane": Chem.MolFromSmarts("[#6][F,Cl,Br,I]"),
+    "Hydrazine": Chem.MolFromSmarts("[NX3][NX3]"),
+    "Hydrazone": Chem.MolFromSmarts("[NX3][NX2]=[#6]"),
+    "Imide": Chem.MolFromSmarts("[CX3](=[OX1])[NX3][CX3](=[OX1])"),
+    "Imine": Chem.MolFromSmarts("[$([CX3]([#6])[#6]),$([CX3H][#6])]=[$([NX2][#6]),$([NX2H])]"),
+    "Isocyanate": Chem.MolFromSmarts("[NX2]=[C]=[O]"),
+    "Isothiocyanate": Chem.MolFromSmarts("[NX2]=[C]=[S]"),
+    "Ketone": Chem.MolFromSmarts("[#6][CX3](=O)[#6]"),
+    "Nitrile": Chem.MolFromSmarts("[NX1]#[CX2]"),
+    "Phenol": Chem.MolFromSmarts("[OX2H][cX3]:[c]"),
+    "Phosphine": Chem.MolFromSmarts("[PX3]"),
+    "Sulfide": Chem.MolFromSmarts("[#16X2H0]"),
+    "Sulfonamide": Chem.MolFromSmarts("[#16X4]([NX3])(=[OX1])(=[OX1])[#6]"),
+    "Sulfonate": Chem.MolFromSmarts("[#16X4](=[OX1])(=[OX1])([#6])[OX2H0]"),
+    "Sulfone": Chem.MolFromSmarts("[#16X4](=[OX1])(=[OX1])([#6])[#6]"),
+    "Sulfonic acid": Chem.MolFromSmarts("[#16X4](=[OX1])(=[OX1])([#6])[OX2H]"),
+    "Sulfoxide": Chem.MolFromSmarts("[#16X3]=[OX1]"),
+    "Thial": Chem.MolFromSmarts("[CX3H1](=S)[#6,H]"),
+    "Thioamide": Chem.MolFromSmarts("[NX3][CX3]=[SX1]"),
+    "Thiol": Chem.MolFromSmarts("[#16X2H]"),
 }
 
 
@@ -72,9 +72,10 @@ def match_group(mol: Chem.Mol, func_group) -> int:
         n = func_group(mol)
     return 0 if n == 0 else 1
 
+
 def get_functional_groups(smiles: str) -> dict:
-    RDLogger.DisableLog('rdApp.*')
-    smiles = smiles.strip().replace(' ', '')
+    RDLogger.DisableLog("rdApp.*")
+    smiles = smiles.strip().replace(" ", "")
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         return None
@@ -95,40 +96,34 @@ def train_model(X_train, y_train, X_test, num_fgs, aug, num, weighted):
     input_tensor = Input(shape=input_shape)
 
     # 1st CNN layer.
-    x = Conv1D(filters=31,
-               kernel_size=(11),
-               strides=1,
-               padding='same')(input_tensor)
+    x = Conv1D(filters=31, kernel_size=(11), strides=1, padding="same")(input_tensor)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation("relu")(x)
     x = MaxPooling1D(pool_size=2, strides=2)(x)
 
     # 2nd CNN layer.
-    x = Conv1D(filters=62,
-       kernel_size=(11),
-       strides=1,
-       padding='same')(x)
+    x = Conv1D(filters=62, kernel_size=(11), strides=1, padding="same")(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation("relu")(x)
     x = MaxPooling1D(pool_size=2, strides=2)(x)
 
     # Flatten layer.
     x = Flatten()(x)
 
     # 1st dense layer.
-    x = Dense(4927, activation='relu')(x)
+    x = Dense(4927, activation="relu")(x)
     x = Dropout(0.48599073736368)(x)
 
     # 2nd dense layer.
-    x = Dense(2785, activation='relu')(x)
+    x = Dense(2785, activation="relu")(x)
     x = Dropout(0.48599073736368)(x)
 
     # 3rd dense layer.
-    x = Dense(1574, activation='relu')(x)
+    x = Dense(1574, activation="relu")(x)
     x = Dropout(0.48599073736368)(x)
 
-    output_tensor = Dense(num_fgs, activation='sigmoid')(x)
-    print('Model Construction')
+    output_tensor = Dense(num_fgs, activation="sigmoid")(x)
+    print("Model Construction")
     model = Model(inputs=input_tensor, outputs=output_tensor)
     model.summary()
     optimizer = Adam()
@@ -140,7 +135,10 @@ def train_model(X_train, y_train, X_test, num_fgs, aug, num, weighted):
             weights = np.zeros((2, number_dim))
             # Calculates weights for each label in a for loop.
             for i in range(number_dim):
-                weights_n, weights_p = (y_train.shape[0]/(2 * (y_train[:,i] == 0).sum())), (y_train.shape[0]/(2 * (y_train[:,i] == 1).sum()))
+                weights_n, weights_p = (
+                    (y_train.shape[0] / (2 * (y_train[:, i] == 0).sum())),
+                    (y_train.shape[0] / (2 * (y_train[:, i] == 1).sum())),
+                )
                 # Weights could be log-dampened to avoid extreme weights for extremly unbalanced data.
                 weights[1, i], weights[0, i] = weights_p, weights_n
 
@@ -148,14 +146,19 @@ def train_model(X_train, y_train, X_test, num_fgs, aug, num, weighted):
 
         def get_weighted_loss(weights):
             def weighted_loss(y_true, y_pred):
-                return K.mean((weights[:,0]**(1.0-y_true))*(weights[:,1]**(y_true))*K.binary_crossentropy(y_true, y_pred), axis=-1)
+                return K.mean(
+                    (weights[:, 0] ** (1.0 - y_true))
+                    * (weights[:, 1] ** (y_true))
+                    * K.binary_crossentropy(y_true, y_pred),
+                    axis=-1,
+                )
+
             return weighted_loss
 
         model.compile(optimizer=optimizer, loss=get_weighted_loss(calculate_class_weights(y_train)))
 
     else:
-
-        model.compile(optimizer=optimizer, loss='binary_crossentropy')
+        model.compile(optimizer=optimizer, loss="binary_crossentropy")
 
     def custom_learning_rate_schedular(epoch):
         if epoch < 31:
@@ -165,17 +168,14 @@ def train_model(X_train, y_train, X_test, num_fgs, aug, num, weighted):
         elif 37 <= epoch < 42:
             return 2.5000001187436284e-06
 
-    print('Start training')
+    print("Start training")
     # Start training.
 
     prediction = model.predict(X_test)
     return (prediction > 0.5).astype(int)
 
 
-
 def interpolate_to_600(spec):
-
-
     old_x = np.arange(len(spec))
     new_x = np.linspace(min(old_x), max(old_x), 600)
 
@@ -183,10 +183,11 @@ def interpolate_to_600(spec):
     new_spec = interp(new_x)
     return new_spec
 
+
 def make_msms_spectrum(spectrum):
     msms_spectrum = np.zeros(10000)
     for peak in spectrum:
-        peak_pos = int(peak[0]*10)
+        peak_pos = int(peak[0] * 10)
         if peak_pos >= 10000:
             peak_pos = 9999
 
@@ -201,22 +202,21 @@ def make_msms_spectrum(spectrum):
 @click.option("--column", type=str, required=False)
 @click.option("--seed", type=int, default=3245)
 def main(analytical_data, out_path, column, seed):
-
     print("Loading Data")
     training_data = None
 
-    if column == 'pos_msms':
-        column = 'msms_positive_40ev'
-    elif column == 'neg_msms':
-        column = 'msms_negative_40ev'
+    if column == "pos_msms":
+        column = "msms_positive_40ev"
+    elif column == "neg_msms":
+        column = "msms_negative_40ev"
 
     for i, parquet_file in enumerate(analytical_data.glob("*.parquet")):
-        data = pd.read_parquet(parquet_file, columns=[column, 'smiles'])
+        data = pd.read_parquet(parquet_file, columns=[column, "smiles"])
 
-        if 'msms' in column:
+        if "msms" in column:
             data[column] = data[column].map(make_msms_spectrum)
 
-        data['func_group'] = data.smiles.map(get_functional_groups)
+        data["func_group"] = data.smiles.map(get_functional_groups)
         data[column] = data[column].map(interpolate_to_600)
 
         if training_data is None:
@@ -230,19 +230,18 @@ def main(analytical_data, out_path, column, seed):
     train, test = train_test_split(training_data, test_size=0.1, random_state=seed)
 
     X_train = np.stack(train[column].to_list())
-    y_train = np.stack(train['func_group'].to_list())
+    y_train = np.stack(train["func_group"].to_list())
     X_test = np.stack(test[column].to_list())
-    y_test = np.stack(test['func_group'].to_list())
+    y_test = np.stack(test["func_group"].to_list())
 
     # Train extended model.
-    prediction = train_model(X_train, y_train, X_test, 37, 'e', 0, 0)
+    prediction = train_model(X_train, y_train, X_test, 37, "e", 0, 0)
 
-    print(f1_score(y_test, prediction, average='micro'))
+    print(f1_score(y_test, prediction, average="micro"))
 
     with open(out_path / "results.pickle", "wb") as file:
-        pickle.dump({'pred': prediction, 'tgt': y_test}, file)
+        pickle.dump({"pred": prediction, "tgt": y_test}, file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- benchmark/xgb/scripts/evaluate_results.py
+++ benchmark/xgb/scripts/evaluate_results.py
@@ -17,20 +17,21 @@ from sklearn.metrics import (
 def load_results(model_dir):
     results = {}
 
-    pickle_files = list(Path(model_dir).glob('*.pickle'))
+    pickle_files = list(Path(model_dir).glob("*.pickle"))
 
     for pickle_file in pickle_files:
         try:
-            with open(pickle_file, 'rb') as f:
+            with open(pickle_file, "rb") as f:
                 data = pickle.load(f)
 
-                if isinstance(data, dict) and 'pred' in data and 'tgt' in data:
-                    results['predictions'] = data['pred']
-                    results['targets'] = data['tgt']
+                if isinstance(data, dict) and "pred" in data and "tgt" in data:
+                    results["predictions"] = data["pred"]
+                    results["targets"] = data["tgt"]
         except Exception as e:
             print(f"Warning: Could not load {pickle_file.name}: {e}")
 
     return results
+
 
 def calculate_metrics(predictions, targets):
     metrics = {}
@@ -45,23 +46,27 @@ def calculate_metrics(predictions, targets):
         pred_classes = pred
         tgt_classes = tgt
 
-    metrics['accuracy'] = accuracy_score(tgt_classes, pred_classes)
-    metrics['f1_macro'] = f1_score(tgt_classes, pred_classes, average='macro', zero_division=0)
-    metrics['f1_micro'] = f1_score(tgt_classes, pred_classes, average='micro', zero_division=0)
-    metrics['precision'] = precision_score(tgt_classes, pred_classes, average='macro', zero_division=0)
-    metrics['recall'] = recall_score(tgt_classes, pred_classes, average='macro', zero_division=0)
+    metrics["accuracy"] = accuracy_score(tgt_classes, pred_classes)
+    metrics["f1_macro"] = f1_score(tgt_classes, pred_classes, average="macro", zero_division=0)
+    metrics["f1_micro"] = f1_score(tgt_classes, pred_classes, average="micro", zero_division=0)
+    metrics["precision"] = precision_score(
+        tgt_classes, pred_classes, average="macro", zero_division=0
+    )
+    metrics["recall"] = recall_score(tgt_classes, pred_classes, average="macro", zero_division=0)
 
     return metrics, pred_classes, tgt_classes
 
+
 def print_summary(model_name, metrics):
-    print(f"\n{'='*80}")
+    print(f"\n{'=' * 80}")
     print(f"{model_name}")
-    print(f"{'='*80}")
+    print(f"{'=' * 80}")
     print(f"  Accuracy:       {metrics['accuracy']:.4f}")
     print(f"  F1 Score (macro): {metrics['f1_macro']:.4f}")
     print(f"  F1 Score (micro): {metrics['f1_micro']:.4f}")
     print(f"  Precision:      {metrics['precision']:.4f}")
     print(f"  Recall:         {metrics['recall']:.4f}")
+
 
 def plot_confusion_matrices(results_dict, output_dir):
     n_models = len(results_dict)
@@ -69,30 +74,31 @@ def plot_confusion_matrices(results_dict, output_dir):
         return
 
     fig, axes = plt.subplots(2, 3, figsize=(18, 12))
-    fig.suptitle('CNN Model Confusion Matrices', fontsize=16)
+    fig.suptitle("CNN Model Confusion Matrices", fontsize=16)
     axes = axes.flatten()
 
     for idx, (model_name, data) in enumerate(results_dict.items()):
         if idx >= 6:
             break
 
-        pred_classes = data['pred_classes']
-        tgt_classes = data['tgt_classes']
+        pred_classes = data["pred_classes"]
+        tgt_classes = data["tgt_classes"]
 
         cm = confusion_matrix(tgt_classes, pred_classes)
 
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=axes[idx], cbar=True)
-        axes[idx].set_title(f'{model_name}')
-        axes[idx].set_ylabel('True Label')
-        axes[idx].set_xlabel('Predicted Label')
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=axes[idx], cbar=True)
+        axes[idx].set_title(f"{model_name}")
+        axes[idx].set_ylabel("True Label")
+        axes[idx].set_xlabel("Predicted Label")
 
     for idx in range(n_models, 6):
-        axes[idx].axis('off')
+        axes[idx].axis("off")
 
     plt.tight_layout()
-    plt.savefig(output_dir / 'confusion_matrices.png', dpi=300, bbox_inches='tight')
+    plt.savefig(output_dir / "confusion_matrices.png", dpi=300, bbox_inches="tight")
     print(f"Saved confusion matrices to {output_dir / 'confusion_matrices.png'}")
     plt.close()
+
 
 def plot_metrics_comparison(results_dict, output_dir):
     model_names = []
@@ -102,70 +108,79 @@ def plot_metrics_comparison(results_dict, output_dir):
     recalls = []
 
     for model_name, data in results_dict.items():
-        metrics = data['metrics']
+        metrics = data["metrics"]
         model_names.append(model_name)
-        accuracies.append(metrics['accuracy'])
-        f1_scores.append(metrics['f1_macro'])
-        precisions.append(metrics['precision'])
-        recalls.append(metrics['recall'])
+        accuracies.append(metrics["accuracy"])
+        f1_scores.append(metrics["f1_macro"])
+        precisions.append(metrics["precision"])
+        recalls.append(metrics["recall"])
 
     fig, axes = plt.subplots(2, 2, figsize=(15, 10))
-    fig.suptitle('CNN Model Performance Comparison', fontsize=16)
+    fig.suptitle("CNN Model Performance Comparison", fontsize=16)
 
     x = np.arange(len(model_names))
     width = 0.6
 
-    axes[0, 0].bar(x, accuracies, width, color='steelblue', alpha=0.8)
-    axes[0, 0].set_ylabel('Score')
-    axes[0, 0].set_title('Accuracy')
+    axes[0, 0].bar(x, accuracies, width, color="steelblue", alpha=0.8)
+    axes[0, 0].set_ylabel("Score")
+    axes[0, 0].set_title("Accuracy")
     axes[0, 0].set_xticks(x)
-    axes[0, 0].set_xticklabels(model_names, rotation=45, ha='right')
+    axes[0, 0].set_xticklabels(model_names, rotation=45, ha="right")
     axes[0, 0].set_ylim([0, 1])
-    axes[0, 0].grid(axis='y', alpha=0.3)
+    axes[0, 0].grid(axis="y", alpha=0.3)
     for i, v in enumerate(accuracies):
-        axes[0, 0].text(i, v + 0.02, f'{v:.3f}', ha='center', va='bottom', fontsize=9)
+        axes[0, 0].text(i, v + 0.02, f"{v:.3f}", ha="center", va="bottom", fontsize=9)
 
-    axes[0, 1].bar(x, f1_scores, width, color='coral', alpha=0.8)
-    axes[0, 1].set_ylabel('Score')
-    axes[0, 1].set_title('F1 Score (Macro)')
+    axes[0, 1].bar(x, f1_scores, width, color="coral", alpha=0.8)
+    axes[0, 1].set_ylabel("Score")
+    axes[0, 1].set_title("F1 Score (Macro)")
     axes[0, 1].set_xticks(x)
-    axes[0, 1].set_xticklabels(model_names, rotation=45, ha='right')
+    axes[0, 1].set_xticklabels(model_names, rotation=45, ha="right")
     axes[0, 1].set_ylim([0, 1])
-    axes[0, 1].grid(axis='y', alpha=0.3)
+    axes[0, 1].grid(axis="y", alpha=0.3)
     for i, v in enumerate(f1_scores):
-        axes[0, 1].text(i, v + 0.02, f'{v:.3f}', ha='center', va='bottom', fontsize=9)
+        axes[0, 1].text(i, v + 0.02, f"{v:.3f}", ha="center", va="bottom", fontsize=9)
 
-    axes[1, 0].bar(x, precisions, width, color='lightgreen', alpha=0.8)
-    axes[1, 0].set_ylabel('Score')
-    axes[1, 0].set_title('Precision')
+    axes[1, 0].bar(x, precisions, width, color="lightgreen", alpha=0.8)
+    axes[1, 0].set_ylabel("Score")
+    axes[1, 0].set_title("Precision")
     axes[1, 0].set_xticks(x)
-    axes[1, 0].set_xticklabels(model_names, rotation=45, ha='right')
+    axes[1, 0].set_xticklabels(model_names, rotation=45, ha="right")
     axes[1, 0].set_ylim([0, 1])
-    axes[1, 0].grid(axis='y', alpha=0.3)
+    axes[1, 0].grid(axis="y", alpha=0.3)
     for i, v in enumerate(precisions):
-        axes[1, 0].text(i, v + 0.02, f'{v:.3f}', ha='center', va='bottom', fontsize=9)
+        axes[1, 0].text(i, v + 0.02, f"{v:.3f}", ha="center", va="bottom", fontsize=9)
 
-    axes[1, 1].bar(x, recalls, width, color='plum', alpha=0.8)
-    axes[1, 1].set_ylabel('Score')
-    axes[1, 1].set_title('Recall')
+    axes[1, 1].bar(x, recalls, width, color="plum", alpha=0.8)
+    axes[1, 1].set_ylabel("Score")
+    axes[1, 1].set_title("Recall")
     axes[1, 1].set_xticks(x)
-    axes[1, 1].set_xticklabels(model_names, rotation=45, ha='right')
+    axes[1, 1].set_xticklabels(model_names, rotation=45, ha="right")
     axes[1, 1].set_ylim([0, 1])
-    axes[1, 1].grid(axis='y', alpha=0.3)
+    axes[1, 1].grid(axis="y", alpha=0.3)
     for i, v in enumerate(recalls):
-        axes[1, 1].text(i, v + 0.02, f'{v:.3f}', ha='center', va='bottom', fontsize=9)
+        axes[1, 1].text(i, v + 0.02, f"{v:.3f}", ha="center", va="bottom", fontsize=9)
 
     plt.tight_layout()
-    plt.savefig(output_dir / 'metrics_comparison.png', dpi=300, bbox_inches='tight')
+    plt.savefig(output_dir / "metrics_comparison.png", dpi=300, bbox_inches="tight")
     print(f"Saved metrics comparison to {output_dir / 'metrics_comparison.png'}")
     plt.close()
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Evaluate CNN model results')
-    parser.add_argument('--models_dir', type=str, default='./benchmark/cnn/models',
-                       help='Directory containing model subdirectories')
-    parser.add_argument('--output_dir', type=str, default='./benchmark/cnn/results',
-                       help='Directory to save evaluation results')
+    parser = argparse.ArgumentParser(description="Evaluate CNN model results")
+    parser.add_argument(
+        "--models_dir",
+        type=str,
+        default="./benchmark/cnn/models",
+        help="Directory containing model subdirectories",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="./benchmark/cnn/results",
+        help="Directory to save evaluation results",
+    )
 
     args = parser.parse_args()
 
@@ -173,26 +188,25 @@ def main():
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    print("\n" + "="*80)
+    print("\n" + "=" * 80)
     print("CNN Model Evaluation Summary")
-    print("="*80)
+    print("=" * 80)
 
     results_dict = {}
-    for model_path in sorted(models_dir.glob('*')):
+    for model_path in sorted(models_dir.glob("*")):
         if model_path.is_dir():
             model_name = model_path.name
             results = load_results(model_path)
 
-            if 'predictions' in results and 'targets' in results:
+            if "predictions" in results and "targets" in results:
                 metrics, pred_classes, tgt_classes = calculate_metrics(
-                    results['predictions'],
-                    results['targets']
+                    results["predictions"], results["targets"]
                 )
 
                 results_dict[model_name] = {
-                    'metrics': metrics,
-                    'pred_classes': pred_classes,
-                    'tgt_classes': tgt_classes
+                    "metrics": metrics,
+                    "pred_classes": pred_classes,
+                    "tgt_classes": tgt_classes,
                 }
 
                 print_summary(model_name, metrics)
@@ -203,22 +217,22 @@ def main():
         return
 
     # Create visualizations
-    print(f"\n{'='*80}")
+    print(f"\n{'=' * 80}")
     print("Generating Visualizations")
-    print("="*80 + "\n")
+    print("=" * 80 + "\n")
 
     plot_confusion_matrices(results_dict, output_dir)
     plot_metrics_comparison(results_dict, output_dir)
 
     # Save summary to text file
-    summary_file = output_dir / 'summary.txt'
-    with open(summary_file, 'w') as f:
-        f.write("="*80 + "\n")
+    summary_file = output_dir / "summary.txt"
+    with open(summary_file, "w") as f:
+        f.write("=" * 80 + "\n")
         f.write("CNN Model Evaluation Summary\n")
-        f.write("="*80 + "\n\n")
+        f.write("=" * 80 + "\n\n")
 
         for model_name, data in results_dict.items():
-            metrics = data['metrics']
+            metrics = data["metrics"]
             f.write(f"Model: {model_name}\n")
             f.write(f"  Accuracy:       {metrics['accuracy']:.4f}\n")
             f.write(f"  F1 Score (macro): {metrics['f1_macro']:.4f}\n")
@@ -227,10 +241,11 @@ def main():
 
     print(f"Saved summary to {summary_file}")
 
-    print(f"\n{'='*80}")
+    print(f"\n{'=' * 80}")
     print("Evaluation Complete")
     print(f"Results saved to: {output_dir}")
-    print("="*80 + "\n")
+    print("=" * 80 + "\n")
+
 
 if __name__ == "__main__":
     main()
