c4b7c00df364082d2a582792d826fece982812c5
kryezleo <kryezleo@students.zhaw.ch>
2026-02-20 14:31:59 +0100

    remove unused files

--- src/spectroscopy_qml/data/dataset.py
+++
@@ -1,8 +0,0 @@
-"""
-Dataset classes for spectroscopic data.
-
-TODO: Implement PyTorch Dataset classes for:
-- Multi-modal spectroscopy dataset
-- SMILES molecular representation handling
-- Functional group label encoding
-"""
